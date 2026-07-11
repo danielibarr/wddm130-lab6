e0071b9e6ee88c98645864047179a321f25b7590
danielibarr <ddanielibarr@gmail.com>
2026-07-10 22:23:28 -0400

    Initial Lab 6 version

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const path = require("path");
+const { body, validationResult } = require("express-validator");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("form", {
+        errors: [],
+        values: {}
+    });
+});
+
+app.post(
+    "/submit",
+    [
+        body("name")
+            .notEmpty()
+            .withMessage("Name is required."),
+
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required."),
+
+        body("lunch")
+            .notEmpty()
+            .withMessage("Please select lunch."),
+
+        body("tickets")
+            .notEmpty()
+            .withMessage("Please select number of tickets."),
+
+        body("campus")
+            .notEmpty()
+            .withMessage("Please select campus."),
+
+        body("postcode")
+            .matches(/^[A-Z][0-9][A-Z]\s[0-9][A-Z][0-9]$/i)
+            .withMessage("Post code is not valid."),
+
+        body("phone")
+            .matches(/^\(?(\d{3})\)?[\.\-\/\s]?(\d{3})[\.\-\/\s]?(\d{4})$/)
+            .withMessage("Phone number is not valid.")
+    ],
+    (req, res) => {
+
+    const validationErrors = validationResult(req);
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const postcode = req.body.postcode;
+    const lunch = req.body.lunch;
+    const tickets = parseInt(req.body.tickets);
+    const campus = req.body.campus;
+
+    let errors = validationErrors.array().map(error => error.msg);
+
+    if (errors.length > 0) {
+
+        res.render("form", {
+            errors: errors,
+            values: req.body
+        });
+
+    } else {
+
+        let cost = tickets * 100;
+
+        if (lunch === "yes") {
+            cost += 60;
+        }
+
+        let tax = cost * 0.13;
+
+        let total = cost + tax;
+
+        res.render("result", {
+            name,
+            email,
+            lunch,
+            campus,
+            cost,
+            tax,
+            total
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
